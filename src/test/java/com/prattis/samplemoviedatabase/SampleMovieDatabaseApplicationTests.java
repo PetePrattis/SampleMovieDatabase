8a79d95489cb75c216a0321261a053a5e9b78094
Panagiotis Prattis <panagiotispr1997@gmail.com>
2024-10-07 22:01:02 +0300

    Initial commit

---
+++ src/test/java/com/prattis/samplemoviedatabase/SampleMovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prattis.samplemoviedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMovieDatabaseApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
